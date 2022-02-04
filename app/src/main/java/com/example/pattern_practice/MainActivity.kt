4a25074a078996019faba403f527b3d6f22b04c6
sbs0516 <tumtum@naver.com>
2022-02-05 01:08:03 +0900

    텍스트 누를 때, 하나의 액티비티에 전부 담는 작성

--- app/src/main/java/com/example/pattern_practice/MainActivity.kt
+++ app/src/main/java/com/example/pattern_practice/MainActivity.kt
@@ -2,10 +2,24 @@ package com.example.pattern_practice
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val title = findViewById<TextView>(R.id.title)
+        val subTitle = findViewById<TextView>(R.id.subTitle)
+
+        title.text = "처음 문장"
+        subTitle.text = "처음 확인"
+
+        title.setOnClickListener {
+            title.text = clickTextView()
+        }
+    }
+    private fun clickTextView(): String {
+        return "문장을 눌렀다"
     }
 }
